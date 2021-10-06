5bbc634df7d8c37556f55fa82965db11875d0ab6
devriazul <riazul56@gmail.com>
2021-10-06 22:35:42 +0600

    php advanced 6 completed

--- Day-06/profile.php
+++ Day-06/profile.php
@@ -6,6 +6,14 @@ $pro = $_FILES['profile'];
 
 var_dump($pro);
 
+// $riaz = array('mahmud'=>"28",'Mahfuj'=>"29");
+
+echo $pro['name'];
+echo '</br>';
+echo $pro['type'];
+echo '</br>';
+echo $pro['size'];
+
 ?>
 
 </pre>
