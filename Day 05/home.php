28a1e0a306d4a742dbf06b69873eedff8660699a
devriazul <riazul56@gmail.com>
2021-10-06 21:11:14 +0600

    php redirect url problem solved

--- Day 05/home.php
+++ Day 05/home.php
@@ -1,10 +1,11 @@
 <?php
-
-echo $user = $_REQUEST['username'];
-echo $pass = $_REQUEST['password'];
+ $user = $_REQUEST['username'];
+ $pass = $_REQUEST['password'];
 
 if ($user=="fastitbd"){
     header("location: https://www.fastitbd.com");
-}elseif('$user=="riaz'){
+}elseif($user=="riaz"){
     header("location: https://riaz.fastitbd.com");
+}else{
+    echo "not matching.";
 }
