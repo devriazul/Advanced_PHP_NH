d842dddddc32ed538adb3548b09951d0bb40f08c
devriazul <riazul56@gmail.com>
2021-10-06 22:46:39 +0600

    php advanced 7 started

---
+++ Day-07/profile.php
@@ -0,0 +1,17 @@
+<pre>
+
+<?php
+
+$pro = $_FILES['profile'];
+
+echo $name = $pro ['name'];
+echo "</br>";
+echo $type = $pro ['type'];
+echo "</br>";
+echo $tmpName = $pro ['tmp_name'];
+echo "</br>";
+echo $size = $pro ['size'];
+
+?>
+
+</pre>
