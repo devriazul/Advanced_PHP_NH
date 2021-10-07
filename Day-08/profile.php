60dde80cbeb62e6ddc54402d0320c38541e22485
devriazul <riazul56@gmail.com>
2021-10-07 19:00:03 +0600

    condition 2

--- Day-08/profile.php
+++ Day-08/profile.php
@@ -5,7 +5,7 @@ $userEmail = $_REQUEST['email'];
 $userPassword = $_REQUEST['password'];
 
 $countPassword = strlen($userPassword);
-    if($countPassword==5){
+    if($countPassword>=5 && $countPassword <=10){
         echo "Perfect";
     }else{
         echo "Wrong";
