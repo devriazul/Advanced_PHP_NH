8e96505fe96b518b6c05fdef2ed257b0c01dc088
devriazul <riazul56@gmail.com>
2021-10-08 23:05:45 +0600

    class 9 error

--- Day-09/profile.php
+++ Day-09/profile.php
@@ -4,8 +4,12 @@ $userName = $_REQUEST['username'];
 $userEmail = $_REQUEST['email'];
 $userPassword = $_REQUEST['password'];
 
-$hash_format = "$2a$07$";
-$salt = "sadfjl;kafaldjsdk1f22";
+$hash_format = "$2y$07$";
+$salt = "sadfjlkafaldjsdk1ff22";
 $conC = $hash_format . $salt;
+
+echo $userPassword;
+echo "<br>";
+echo crypt($userPassword,$conC);
 
 ?>
