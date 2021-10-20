72dd3a4edd0072e9ca10660cfb156be572aeecab
devriazul <riazul56@gmail.com>
2021-10-20 18:35:36 +0600

    cookies and session 20sec

---
+++ Day-11/index.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.2/dist/css/bootstrap.min.css" rel="stylesheet"
+        integrity="sha384-uWxY/CJNBR+1zjPWmfnSnVxwRheevXITnMqoEIeG1LJrdI0GlVs/9cVSyPYXdcSF" crossorigin="anonymous">
+</head>
+
+<body>
+    <div class="container">
+        <div class="row mt-5">
+            <div class="col-md-3"></div>
+            <div class="col-md-6">
+                <?php
+
+                    $name= "user";
+                    $value = "Riazul";
+
+                    setcookie($name, $value, time()+20);
+
+                ?>
+                
+            </div>
+        </div>
+    </div>
+</body>
+
+</html>
